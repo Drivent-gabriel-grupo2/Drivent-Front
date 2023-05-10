129e753a7dc7c90380161416142b5170e02df8b3
Antonio-Jefferson <antjeffersonbatista@gmail.com>
2023-05-10 14:24:50 -0300

    feat: create structure

--- src/pages/Dashboard/Payment/index.js
+++ src/pages/Dashboard/Payment/index.js
@@ -1,3 +1,35 @@
 export default function Payment() {
-  return 'Pagamento: Em breve!';
+  return (
+    <>
+      <h1>Ingresso e pagamento</h1> 
+      <p>Primeiro, escolha sua modalidade de ingresso</p>
+      <div>
+        <div>
+          <p>Presencial</p>
+          <span>R$ 250</span>
+        </div>
+        <div>
+          <p>Online</p>
+          <span>R$ 100</span>
+        </div>
+      </div>
+      <div>
+        <p>Ótimo! Agora escolha sua modalidade de hospedagem</p>
+        <div>
+          <div>
+            <p>Sem Hotel</p>
+            <span>R$ 0</span>
+          </div>
+          <div>
+            <p>Com Hotel</p>
+            <span>+ R$ 350</span>
+          </div>
+        </div>
+      </div>
+      <div>
+        <p>Fechado! O total ficou em R$ 600. Agora é só confirmar:</p>
+        <
+      </div>
+    </>
+  );
 }
