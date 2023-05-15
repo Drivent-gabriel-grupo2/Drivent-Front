840f3edb30f5796499b4d9a9b1d7a4fa9a43b894
encarnacao <cencarnacaoq@gmail.com>
2023-05-14 23:23:43 -0300

    refactor: rename function names

--- src/hooks/api/useTicket.js
+++ src/hooks/api/useTicket.js
@@ -2,7 +2,7 @@ import useAsync from '../useAsync';
 import useToken from '../useToken';
 import { getTicketTypes } from '../../services/ticktsApi';
 
-export function useTicket() {
+export function useTicketTypes() {
   const token = useToken();
   const ticketsTypes = useAsync(() => getTicketTypes(token));
 
