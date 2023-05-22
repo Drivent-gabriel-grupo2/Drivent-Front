b7dc35d15ad3c21df898cee2de6db64d6eff1790
encarnacao <cencarnacaoq@gmail.com>
2023-05-22 15:31:18 -0300

    feat: use try/catch to API requests

--- src/services/hotelApi.js
+++ src/services/hotelApi.js
@@ -1,11 +1,15 @@
 import api from './api';
 
 export async function getHotels(token) {
-  const response = await api.get('/hotels', {
-    headers: {
-      Authorization: `Bearer ${token}`,
-    },
-  });
-  
-  return response.data;
+  try{
+    const response = await api.get('/hotels', {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    
+    return response.data;
+  } catch{
+    return null;
+  }
 };
